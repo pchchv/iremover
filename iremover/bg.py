17510cfbb9870f688e092a3dd515f490177fb628
pchchv <ipchchv@gmail.com>
2023-03-23 12:10:49 +0300

    Add a alpha_matting_cutout func

--- iremover/bg.py
+++ iremover/bg.py
@@ -2,6 +2,10 @@ import numpy as np
 from PIL import Image
 from enum import Enum
 from PIL.Image import Image as PILImage
+from scipy.ndimage import binary_erosion
+from pymatting.util.util import stack_images
+from pymatting.alpha.estimate_alpha_cf import estimate_alpha_cf
+from pymatting.foreground.estimate_foreground_ml import estimate_foreground_ml
 from cv2 import getStructuringElement, MORPH_OPEN, MORPH_ELLIPSE, BORDER_DEFAULT, morphologyEx, GaussianBlur
 
 kernel = getStructuringElement(MORPH_ELLIPSE, (3, 3))
@@ -29,4 +33,46 @@ def post_process(mask: np.ndarray) -> np.ndarray:
 def naive_cutout(img: PILImage, mask: PILImage) -> PILImage:
     empty = Image.new("RGBA", (img.size), 0)
     cutout = Image.composite(img, empty, mask)
+    return cutout
+
+
+def alpha_matting_cutout(
+    img: PILImage,
+    mask: PILImage,
+    foreground_threshold: int,
+    background_threshold: int,
+    erode_structure_size: int,
+) -> PILImage:
+    if img.mode == "RGBA" or img.mode == "CMYK":
+        img = img.convert("RGB")
+
+    img = np.asarray(img)
+    mask = np.asarray(mask)
+
+    is_foreground = mask > foreground_threshold
+    is_background = mask < background_threshold
+
+    structure = None
+    if erode_structure_size > 0:
+        structure = np.ones(
+            (erode_structure_size, erode_structure_size), dtype=np.uint8
+        )
+
+    is_foreground = binary_erosion(is_foreground, structure=structure)
+    is_background = binary_erosion(is_background, structure=structure, border_value=1)
+
+    trimap = np.full(mask.shape, dtype=np.uint8, fill_value=128)
+    trimap[is_foreground] = 255
+    trimap[is_background] = 0
+
+    img_normalized = img / 255.0
+    trimap_normalized = trimap / 255.0
+
+    alpha = estimate_alpha_cf(img_normalized, trimap_normalized)
+    foreground = estimate_foreground_ml(img_normalized, alpha)
+    cutout = stack_images(foreground, alpha)
+
+    cutout = np.clip(cutout * 255, 0, 255).astype(np.uint8)
+    cutout = Image.fromarray(cutout)
+
     return cutout
