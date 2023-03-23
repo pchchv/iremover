4d292ff7703b1a1bde0cf55b800d80100f4d592d
pchchv <ipchchv@gmail.com>
2023-03-23 12:31:41 +0300

    Implement a click options

--- iremover/cli.py
+++ iremover/cli.py
@@ -1,7 +1,80 @@
-from iremover import __version__
+import sys
 import click
+from typing import IO
+from iremover.bg import remove
+from iremover import __version__
+from iremover.session import new_session
 
 @click.group()
 @click.version_option(version=__version__)
 def main() -> None:
     pass
+
+
+@main.command(help="for a file as input")
+@click.option(
+    "-m",
+    "--model",
+    default="u2net",
+    type=click.Choice(
+        ["u2net", "u2netp", "u2net_human_seg", "u2net_cloth_seg", "silueta"]
+    ),
+    show_default=True,
+    show_choices=True,
+    help="model name",
+)
+@click.option(
+    "-a",
+    "--alpha-matting",
+    is_flag=True,
+    show_default=True,
+    help="use alpha matting",
+)
+@click.option(
+    "-af",
+    "--alpha-matting-foreground-threshold",
+    default=240,
+    type=int,
+    show_default=True,
+    help="trimap fg threshold",
+)
+@click.option(
+    "-ab",
+    "--alpha-matting-background-threshold",
+    default=10,
+    type=int,
+    show_default=True,
+    help="trimap bg threshold",
+)
+@click.option(
+    "-ae",
+    "--alpha-matting-erode-size",
+    default=10,
+    type=int,
+    show_default=True,
+    help="erode size",
+)
+@click.option(
+    "-om",
+    "--only-mask",
+    is_flag=True,
+    show_default=True,
+    help="output only the mask",
+)
+@click.option(
+    "-ppm",
+    "--post-process-mask",
+    is_flag=True,
+    show_default=True,
+    help="post process the mask",
+)
+@click.argument(
+    "input", default=(None if sys.stdin.isatty() else "-"), type=click.File("rb")
+)
+@click.argument(
+    "output",
+    default=(None if sys.stdin.isatty() else "-"),
+    type=click.File("wb", lazy=True),
+)
+def i(model: str, input: IO, output: IO, **kwargs) -> None:
+    output.write(remove(input.read(), session=new_session(model), **kwargs))
