d7c055b149cfc9d82e44414c42dc79f78b30bbf0
pchchv <ipchchv@gmail.com>
2023-03-23 12:03:28 +0300

    Add a post_process func

--- iremover/bg.py
+++ iremover/bg.py
@@ -1,7 +1,24 @@
+import numpy as np
 from enum import Enum
+from cv2 import getStructuringElement, MORPH_OPEN, MORPH_ELLIPSE, BORDER_DEFAULT, morphologyEx, GaussianBlur
+
+kernel = getStructuringElement(MORPH_ELLIPSE, (3, 3))
 
 
 class ReturnType(Enum):
     BYTES = 0
     PILLOW = 1
     NDARRAY = 2
+
+
+def post_process(mask: np.ndarray) -> np.ndarray:
+    """
+    Post Process the mask for a smooth boundary by applying Morphological Operations
+    Based on paper: https://www.sciencedirect.com/science/article/pii/S2352914821000757
+    args:
+        mask: Binary Numpy Mask
+    """
+    mask = morphologyEx(mask, MORPH_OPEN, kernel)
+    mask = GaussianBlur(mask, (5, 5), sigmaX=2, sigmaY=2, borderType=BORDER_DEFAULT)
+    mask = np.where(mask < 127, 0, 255).astype(np.uint8)  # convert again to binary
+    return mask
